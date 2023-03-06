d45066072846fa634dab5825cc73ba35d703fca2
Panupong Rong-O <tayalone@gmail.com>
2023-03-06 15:18:20 +0700

    ✨ add router

--- main.go
+++ main.go
@@ -19,5 +19,59 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	r.GET("/posts/:id", func(c *gin.Context) {
+		type Params struct {
+			ID uint `uri:"id" binding:"required"`
+		}
+
+		var p Params
+		if err := c.ShouldBindUri(&p); err != nil {
+			c.JSON(400, gin.H{"msg": err})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+			"id":      p.ID,
+		})
+	})
+
+	r.GET("/posts/:id/comments", func(c *gin.Context) {
+		type Params struct {
+			ID uint `uri:"id" binding:"required"`
+		}
+
+		var p Params
+		if err := c.ShouldBindUri(&p); err != nil {
+			c.JSON(400, gin.H{"msg": err})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+			"id":      p.ID,
+		})
+	})
+
+	r.GET("/posts/:id/comments/:commentId", func(c *gin.Context) {
+		type Params struct {
+			ID        uint `uri:"id" binding:"required"`
+			CommentID uint `uri:"commentId" binding:"required"`
+		}
+
+		var p Params
+		if err := c.ShouldBindUri(&p); err != nil {
+			c.JSON(400, gin.H{"msg": err})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message":   "pong",
+			"id":        p.ID,
+			"commentId": p.CommentID,
+		})
+	})
+
 	r.Run(":" + config.Port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
