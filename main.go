54253b97f7f1b969b928a700cf2058e257307eec
Panupong Rong-O <tayalone@gmail.com>
2023-03-06 14:43:59 +0700

    ➕ add package gin

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	Config "github.com/tayalone/ma-go-wire/config"
 )
 
@@ -10,4 +12,12 @@ func main() {
 	fmt.Println("Start ma go wire")
 	config := Config.Initialize()
 	fmt.Println("config.Port", config.Port)
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":" + config.Port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
